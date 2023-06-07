dfe9feb483a174d492ce74dfd6cbadcf5ee93a54
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-06-07 22:28:23 +0100

    add README.md file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { registerUser } from '../services/api';
+import { registerUser, login } from '../services/api';
 
 const Register = () => {
   const [email, setEmail] = useState('');
